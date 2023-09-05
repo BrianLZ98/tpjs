40e890353042a85e611b606552bad128b9c69eef
BrianLZ98 <ZapatabrianL@hotmail.com>
2023-09-05 19:19:03 -0300

    primer commit

---
+++ index.js
@@ -0,0 +1,14 @@
+//ejercicios de javascript
+let Nombre = "Brian" ;
+let Apellido = "Zapata";
+let NombreDeUsuario = "Brian10";
+let Edad = 25;
+let Correo = "zapatabrian51@gmail.com";
+let MayorDeEdad = true ;
+let Sueldo = 1000;
+let Gastos = 500;
+
+let NombreCompleto =   Nombre + Apellido;
+alert(NombreCompleto);
+alert("el nombre de usuario es: " + NombreDeUsuario);
+alert("el  sueldo de la persona es: " + (Sueldo - Gastos));
